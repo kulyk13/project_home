47ed196cee108e615d04111898b8a4c2b76df0a3
kulyk13 <13dimon1313@gmail.com>
2021-12-18 18:02:35 +0200

    stylizing hero section

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//Hero slider
+const slides = ['01', '02']
+const heroSlider = new Swiper(".hero-slider", {
+    loop: true,
+    spaceBetween: 100,
+    speed: 400,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+            clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (slides[index++]) + '</span>';
+        }
+    },
+});
